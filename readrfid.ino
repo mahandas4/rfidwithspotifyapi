d415384d0f4695803ad00d2eb9b263d4f21f93b0
Mahan Das <119815198+mahandas4@users.noreply.github.com>
2022-12-04 20:58:49 +0000

    Update readrfid.ino

--- readrfid.ino
+++ readrfid.ino
@@ -55,12 +55,6 @@ void loop() {
   rfid.PCD_StopCrypto1();
 }
 
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
 
 void printDec(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
